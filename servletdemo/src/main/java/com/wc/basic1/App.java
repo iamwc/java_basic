551e15c20c3ea9e74a8bee6ee66e832ec605e644
vin <vin_wang@wistronits.com>
2020-07-19 21:24:12 +0800

    init

---
+++ servletdemo/src/main/java/com/wc/basic1/App.java
@@ -0,0 +1,13 @@
+package com.wc.basic1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
